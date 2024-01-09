9b68e82190bc1fa6731ae8344688dcc32c24f3e3
leerobertdyer <leerobertdyer@gmail.com>
2024-01-09 12:12:04 -0500

    added contact and about me

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 const mainDiv = document.getElementById('mainDiv')
 mainDiv.addEventListener('click', () => {console.log(window.innerWidth)})
+
+const projects = document.getElementsByClassName('project')
+console.log(projects);
