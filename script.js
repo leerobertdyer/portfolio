226b9380f0d59b24702f11c9635fa8db2f02f49c
leerobertdyer <leerobertdyer@gmail.com>
2024-01-16 22:58:39 -0500

    changed to signup form

--- script.js
+++ script.js
@@ -1,6 +1,39 @@
-const mainDiv = document.getElementById('mainDiv')
-mainDiv.addEventListener('click', () => {console.log(window.innerWidth)})
+const contact = document.getElementById('contact')
 
-const projects = document.getElementsByClassName('project')
-console.log(projects);
+const sender = document.getElementById('email')
+const message = document.getElementById('message')
+const submitForm = document.getElementById('submitForm')
+submitForm.addEventListener('click', () => sendEmail())
 
+const sendEmail = async() => {
+    if (!sender.value || !message.value) {
+        return
+    }
+    
+    const popup = document.createElement('div')
+    popup.className="popup"
+
+
+    popup.addEventListener('click', () => {
+        sender.value=""
+        message.value=""
+        popup.remove()
+    })
+    contact.appendChild(popup)
+    const success = document.createElement('p')
+    success.innerHTML="Thank you! <br/>Your message has been sent<br/><br/>X"
+    success.className="success"
+    popup.appendChild(success)
+
+    const resp = await fetch('https://portfolio-server-28r2.onrender.com/send-email', {
+        method: "POST",
+        headers: {"Content-Type": "Application/json"},
+        body: JSON.stringify({
+            senderEmail: sender.value,
+            message: message.value
+        })
+    });
+    if (resp.ok) {
+        console.log('message sent');
+    }
+}
